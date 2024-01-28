885e38aa90b4027022ae64e955e222b3f8bc69cf
Pablo <gsdg.pramirez@gmail.com>
2024-01-28 21:36:23 +0100

    Agregación de validación de DNI

--- assets/js/validation.js
+++ assets/js/validation.js
@@ -27,3 +27,30 @@ document.addEventListener('DOMContentLoaded', () => {
 
 const tooltipTriggerList = document.querySelectorAll('[data-bs-toggle="tooltip"]')
 const tooltipList = [...tooltipTriggerList].map(tooltipTriggerEl => new bootstrap.Tooltip(tooltipTriggerEl))
+
+function validarDNI(dni) {
+    // Expresión regular para validar el formato del DNI
+    var regex = /^\d{8}[a-zA-Z]$/;
+
+    if (!regex.test(dni)) {
+        return false;
+    }
+
+    // Extraer el número y la letra del DNI
+    var numero = dni.slice(0, 8);
+    var letra = dni.slice(8).toUpperCase();
+
+    // Calcular la letra esperada
+    var letras = 'TRWAGMYFPDXBNJZSQVHLCKE';
+    var letraEsperada = letras.charAt(numero % 23);
+    // Comparar la letra calculada con la letra del DNI
+    return letra === letraEsperada;
+}
+
+// Ejemplo de uso
+/*var dniEjemplo = '';
+if (validarDNI(dniEjemplo)) {
+    console.log('El DNI es válido.');
+} else {
+    console.log('El DNI no es válido.');
+}*/
